759897e02c0f5c5d29abbc58e53d284609a96115
Fabiano Leite <fabianoleittes@gmail.com>
2019-03-28 01:01:30 -0300

    Remove old files.

--- tag-fish/functions/pubkey.fish
+++
@@ -1,3 +0,0 @@
-function pubkey -d "Pipe my public key to my clipboard"
-	commandline -a -- "more ~/.ssh/id_rsa.pub | pbcopy | echo '===> Public key copied to pastboard"
-end
--- tag-fish/system.fish
+++
@@ -1,9 +0,0 @@
-# PS
-abbr --add psa="ps aux"
-abbr --add psg="ps aux | grep "
-abbr --add psr='ps aux | grep ruby'
-abbr --add ll="ls -al"
-abbr --add ka9='killall -9'
-abbr --add k9='kill -9'
-abbr --add brewu='brew update & brew upgrade & brew cleanup & brew prune & brew doctor'
-abbr --add fsdev='foreman start -f Procfile.dev'
