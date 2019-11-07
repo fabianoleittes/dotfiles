ff11d9d24ac220e70ec9c60a04f10417db2a4326
Fabiano Leite <fabianoleittes@Fabianos-MacBook-Pro.local>
2019-11-07 14:31:31 -0300

    WIP

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -4,7 +4,7 @@ set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 set VISUAL nvim
 set -U EDITOR $VISUAL
 
-# source ~/dotfiles/tag-fish/config/fish/abbr.fish
+source ~/dotfiles/tag-fish/config/fish/abbr.fish
 source ~/dotfiles/tag-fish/config/fish/git.fish
 
 
